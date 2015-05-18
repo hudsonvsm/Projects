e8450c0c51edf544f17cc95f7c7bb5bc44af7a05
hudsonvsm <hudsonvsm@gmail.com>
2015-05-18 11:09:57 +0300

    update
    Good.

--- Personal-Site/Hudson-Site/Scripts/Home-News-JSON.js
+++ Personal-Site/Hudson-Site/Scripts/Home-News-JSON.js
@@ -11,17 +11,17 @@
                 fontColor: "fg-white",
                 title: "Personal IDE Visual Studio",
                 image: "VisualStudio2013.png",
-                content: "My codding skills are based on Macrosoft Visual Studio. I won VS 2013 Ultimate for my excelent pereformace as a student," +
-                    " durring my training in Telerik Software Academy."
+                content: "My coding skills are based on Microsoft Visual Studio. I won VS 2013 Ultimate for my excellent preformace as a student," +
+                    " during my training in Telerik Software Academy."
             },
             {
                 titleColor: "bg-emerald",
                 fontColor: "fg-white",
-                title: "Pereferred language C#",
+                title: "Preferred language C#",
                 image: "csharp.png",
-                content: "My understanding of C# language is very deep. My grasp of OOP and HQPC are quite vast. " +
-                    "The 4 OOP principles: Inheritance, Abstraction, Encapsulation and Polymorphism are corrner stone of my codding skills. " +
-                    "My abstraction thinkig helps me a lot in constructing HQPC using DRY and SOLID principles implementing design patterns."
+                content: "My understanding of C# language is excellent. My grasp of OOP and HQPC is quite vast. " +
+                    "The 4 OOP principles: Inheritance, Abstraction, Encapsulation and Polymorphism are corner stones of my coding skills. " +
+                    "My abstract thinking helps me a lot in constructing HQPC using DRY and SOLID principles implementing design patterns."
             },
             {
                 titleColor: "bg-lightBlue",
@@ -29,7 +29,7 @@
                 title: "HTML, JS and CSS",
                 image: "html-css-js.png",
                 content: "Those are my pereferred languages for Frond-End development. This site is build on those languages. " +
-                    "I have used Metro UI CSS, JSON for repeating data with Handlebars templating and jQuery, just to make myself happy." +
+                    "I have used Metro UI CSS, JSON for recurring data with Handlebars templating and jQuery, just to make myself happy." +
                     "The source is on Github."
             }
         ]
